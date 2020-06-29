dde4298d174924bb2c73ccde6a751a3def15d3c9
wangxinlei <1241833750@qq.com>
2020-06-29 22:48:42 +0800

    index

--- src/views/Books/OutstandingBooks.vue
+++ src/views/Books/OutstandingBooks.vue
@@ -1,111 +1,69 @@
 <template>
-  <div class="app-container">
-
-    <el-dialog
-      :title="`个人信息`"
-      :visible.sync="showDialog"
-      width="30%"
-      @close="showDialog = false"
+  <el-table
+    :data="tableData"
+    style="width: 100%"
+  >
+    <el-table-column
+      label="名称"
+      width="180"
     >
-      <el-container>
-        <el-aside width="100px" />
-        <el-main>
-          <span>
+      <template slot-scope="scope">
+        <span style="margin-left: 10px">{{ scope.row.name }}</span>
+      </template>
+    </el-table-column>
 
-            <el-avatar
-              shape="square"
+    <el-table-column
+      label="数量"
+      width="80"
+    >
+      <template slot-scope="scope">
+        <span style="margin-left: 10px">{{ scope.row. count }}</span>
+      </template>
+    </el-table-column>
 
-              src="user.headPortrait"
-            /></span>
-          <br><br>
-          <span> <i class="el-icon-user" />
-            真实姓名:{{ user.realName }}</span><br><br>
-          <span>
-            <svg-icon icon-class="sex" />
-            性别:{{ user.sex?user.sex:'未填写' }}</span><br><br>
-          <span> <i class="el-icon-mobile" />
-            手机:{{ user.phone?user.phone:'未填写' }}</span><br><br>
-          <span>
-            <svg-icon icon-class="qq" />
-            QQ:{{ user.qq ?user.qq:'未填写' }}</span><br><br>
+    <el-table-column
+      label="地址"
+      width="80"
+    >
+      <template slot-scope="scope">
+        <span style="margin-left: 10px">{{ scope.row.address }}</span>
+      </template>
+    </el-table-column>
 
-        </el-main>
-        <el-aside width="100px" />
-      </el-container>
-
-    </el-dialog>
-
-  </div>
+    <el-table-column label="操作">
+      <template slot-scope="scope">
+        <el-button
+          size="mini"
+          type="success"
+          @click="handleDelete(scope.$index, scope.row)"
+        >还书</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
-<script lang="ts">
-import { queryUser, forbidUser } from '@/api/manager'
-
+<script>
 export default {
-  name: 'User',
   data() {
     return {
-      data: [],
-      selectUserId: [],
-      queryOptions: {
-        nickName: '',
-        page: 1,
-        pageSize: 10
+      tableData: [{
+        name: '紫禁城的黄昏',
+        count: '1',
+        address: '2 楼'
       },
-      user: {},
-      total: 0,
-      loading: true,
-      showDialog: false
+      {
+        name: '紫禁城的黄昏2',
+        count: '1',
+        address: ' 2楼'
+      }]
     }
   },
-  created() {
-    // this.requestData()
-  },
   methods: {
-    requestData() {
-      queryUser(this.queryOptions).then(res => {
-        if (res.status === 0) {
-          this.data = res.data.list
-          this.total = res.data.total
-          this.loading = false
-        }
-      })
+    handleEdit(index, row) {
+      console.log(index, row)
     },
-    handleSelect(select) {
-      this.selectUserId = select.map(item => {
-        return item.id
-      })
-    },
-    handleForbid(id) {
-      this.$confirm(`确定封禁(解禁)用户吗？`, '提示', {
-        type: 'warning'
-      }).then(() => {
-        forbidUser(id).then(res => {
-          if (res.status === 0) {
-            this.requestData()
-            this.$message({
-              type: 'success'
-              // message: res.msg
-            })
-          }
-        })
-      })
-    },
-    handleFilter() {
-      this.queryOptions.page = 1
-      this.requestData()
-    },
-    handlePersonInfo(user) {
-      this.user = user
-      this.showDialog = true
-    },
-    handleSizeChange(val) {
-      this.queryOptions.pageSize = val
-      this.requestData()
-    },
-    handleCurrentChange(val) {
-      this.queryOptions.page = val
-      this.requestData()
+    handleDelete(index, row) {
+      console.log(index, row)
     }
   }
 }
--- src/views/Books/index.vue
+++ src/views/Books/index.vue
@@ -1,64 +1,116 @@
 <template>
-  <div class="app-container">
+  <el-table
+    :data="message"
+    style="width: 25%"
+  >
+    <el-table-column>
+      <template slot-scope="scope">
 
-    <el-container>
-      <el-header style="background-color: rgb(238, 220, 246)">
-        <span>{{ name }}</span>
-      </el-header>
-      <el-main style="background-color: rgb(238, 241, 246)" width="100px">
-        图书序号:
-        <span>{{ index }}</span>
-        <br>
-        图书名称:
-        <span>{{ name }}</span>
-        <br>
-        作者:
-        <span>{{ author }}</span>
-        <br>
-        出版社:
-        <span>{{ press }}</span>
-        <br>
-        数量:
-        <span>{{ count }}</span>
-        <br>
-        可借数量:
-        <span>{{ reminding }}</span>
-        <br>
-        地址:
-        <span>{{ add }}</span>
-      </el-main>
-    </el-container>
-  </div>
+        <el-popover trigger="click" placement="right">
+          <p>名称: {{ scope.row.name }}</p>
+          <p>作者: {{ scope.row.author }}</p>
+          <p>出版社: {{ scope.row.press }}</p>
+          <p>剩余: {{ scope.row.remind }}</p>
+          <p> bns: {{ scope.row.id }}</p>
+          <p>地址: {{ scope.row.addr }}</p>
+          <div slot="reference" class="name-wrapper">
+            <el-card class="box-card">
+              <div slot="header" class="clearfix">
+                <span>{{ scope.row.name }}</span>
+                <el-button style="float: right; padding: 3px 0" type="text">详细信息</el-button>
+              </div>
+              <div>
+                <p>{{ scope.row.author }}</p>
+                <p>{{ scope.row.press }}</p>
+              </div>
+            </el-card>
+          </div>
+          <el-button type="text" @click="open(scope.$index, scope.row)">借阅</el-button>
+        </el-popover>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <style>
-  .el-header {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: top;
-    line-height: 60px;
+  .text {
+    font-size: 20px;
   }
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: top;
-    line-height: 40px;
+
+  .item {
+    margin-bottom: 10px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: right
+  }
+
+  .box-card {
+    width: 250px;
   }
 </style>
 
-<script lang="ts">
-
+<script>
 export default {
   data() {
     return {
-      index: '1234',
-      name: '我的前半生',
-      author: '溥仪',
-      press: '人民出版社',
-      count: '10',
-      reminding: '5',
-      add: '1楼'
+      message: [
+        {
+          id: '01',
+          name: '我的前半生',
+          author: '爱新觉罗·溥仪',
+          press: '人民出版社',
+          total: '10',
+          remind: '5',
+          addr: '1楼'
 
+        },
+        {
+          id: '02',
+          name: '我的前半生2',
+          author: '溥仪',
+          press: '人民出版社',
+          total: '10',
+          remind: '5',
+          addr: '1楼'
+
+        },
+        {
+          id: '03',
+          name: '我的前半生3',
+          author: '溥仪',
+          press: '人民出版社',
+          total: '10',
+          remind: '5',
+          addr: '1楼'
+
+        }
+      ],
+      visible: false
+    }
+  },
+  methods: {
+    open() {
+      this.$confirm('是否借阅此图书', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'info'
+      }).then(() => {
+        this.$message({
+          type: 'success',
+          message: '借阅成功!'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消'
+        })
+      })
     }
   }
 }
